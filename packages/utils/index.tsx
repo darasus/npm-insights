e96f148e6b3c71087ac9fff23cf254d4281935e9
Ilya Daraseliya <idarase@gmail.com>
2022-10-17 23:02:09 +0200

    fix

--- packages/utils/index.tsx
+++ packages/utils/index.tsx
@@ -1,20 +1,20 @@
-const productionUrl = "https://www.npm-package-size.com";
+const productionUrl = 'https://www.npm-package-size.com'
 
 export function getBaseUrl() {
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
-    return productionUrl;
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+    return productionUrl
   }
 
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
-    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+    return `https://${process.env.VERCEL_URL}`
   }
 
   // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`
 
-  return "";
+  return ''
 }
 
 export function getProductionBaseUrl() {
-  return productionUrl;
+  return productionUrl
 }
