38be942f5428324b61b1ca047163546f785a08b5
Ilya Daraseliya <idarase@gmail.com>
2022-10-25 20:35:47 +0200

    fix link hover

--- packages/ui/src/Link.tsx
+++ packages/ui/src/Link.tsx
@@ -1,19 +1,8 @@
-import NextLink, { LinkProps } from "next/link";
-import { Link as ChakraLink } from "@chakra-ui/react";
-import React from "react";
+import NextLink, { LinkProps } from 'next/link'
+import React from 'react'
 
-type Props = React.PropsWithChildren<LinkProps> & { hoverStyles?: boolean };
+type Props = React.PropsWithChildren<LinkProps> & { hoverStyles?: boolean }
 
 export function Link({ children, hoverStyles = true, ...props }: Props) {
-  return (
-    <NextLink {...props}>
-      <ChakraLink
-        _hover={{
-          textDecoration: hoverStyles ? "underline" : "none",
-        }}
-      >
-        {children}
-      </ChakraLink>
-    </NextLink>
-  );
+  return <NextLink {...props}>{children}</NextLink>
 }
