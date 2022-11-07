7c921527a1226abe574bc9b1fef8e1beabdf314f
Ilya Daraseliya <idarase@gmail.com>
2022-11-07 12:07:51 +0100

    use upstash redis

--- packages/lib/src/cache.ts
+++ packages/lib/src/cache.ts
@@ -1,43 +1,48 @@
-import Redis from "ioredis";
+import { Redis } from '@upstash/redis'
 
 export class CacheService {
-  redis = new Redis(process.env.REDIS_URL as string);
+  redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  })
 
   fetch = async (
     key: string,
     fetcher: () => Promise<string | null>,
     expires: number
   ) => {
-    const existing = await this.get(key);
+    const existing = await this.get(key)
 
-    if (existing !== null) return existing;
+    if (existing !== null) return existing
 
-    return this.set(key, fetcher, expires);
-  };
+    return this.set(key, fetcher, expires)
+  }
 
   get = async (key: string): Promise<string | null> => {
-    const value = await this.redis.get(key);
-    if (value === null) return null;
-    return value;
-  };
+    const value = await this.redis.get<string>(key)
+    if (value === null) return null
+    return value
+  }
 
   set = async (
     key: string,
     fetcher: () => Promise<string | null>,
     expires: number
   ) => {
-    const value = await fetcher();
+    const value = await fetcher()
     if (value) {
-      await this.redis.set(key, value, "EX", expires);
+      await this.redis.set(key, value, {
+        ex: expires,
+      })
     }
-    return value;
-  };
+    return value
+  }
 
   del = async (key: string) => {
-    await this.redis.del(key);
-  };
+    await this.redis.del(key)
+  }
 
   perge = async () => {
-    await this.redis.flushall();
-  };
+    await this.redis.flushall()
+  }
 }
