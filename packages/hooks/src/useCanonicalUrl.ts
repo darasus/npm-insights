b97b59249d28120edd02099b88a64cb38606f5a7
Ilya Daraseliya <idarase@gmail.com>
2023-01-06 20:15:16 +0100

    fix url

--- packages/hooks/src/useCanonicalUrl.ts
+++ packages/hooks/src/useCanonicalUrl.ts
@@ -1,17 +1,17 @@
-import { useRouter } from "next/router";
-import { getProductionBaseUrl } from "utils";
+import { useRouter } from 'next/router'
+import { getProductionBaseUrl } from 'utils'
 
 interface UseCanonicalUrlArgs {
-  slug?: string;
+  slug?: string
 }
 
 export function useCanonicalUrl({ slug }: UseCanonicalUrlArgs) {
-  const router = useRouter();
-  const baseUrl = getProductionBaseUrl();
+  const router = useRouter()
+  const baseUrl = getProductionBaseUrl()
 
-  if (router.pathname.startsWith("/package/") && slug) {
-    return `${baseUrl}/p/${slug}`;
+  if (router.pathname.startsWith('/package/') && slug) {
+    return `${baseUrl}/package/${slug}`
   }
 
-  return `${baseUrl}/`;
+  return `${baseUrl}/`
 }
